9e33157e7989d89e4c9390c8e4f717d28fe54702
Paulo Sergio <pauloserg861@gmail.com>
2022-09-22 14:39:28 -0300

    criando link de navegação para pagina de comentarios e limpando upgrade

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wrapper">
     <side-bar>
+      <!-- menu mobile -->
       <mobile-menu slot="content"></mobile-menu>
+
+      <!-- sidebar structure -->
       <sidebar-link to="/admin/overview">
         <i class="nc-icon nc-chart-pie-35"></i>
         <p>Dashboard</p>
@@ -30,48 +33,49 @@
         <i class="nc-icon nc-bell-55"></i>
         <p>Notifications</p>
       </sidebar-link>
-
-      <template slot="bottom-links">
-        <sidebar-link class="active"
-                      to="/admin/upgrade">
-          <i class="nc-icon nc-alien-33"></i>
-          <p>Upgrade to PRO</p>
-        </sidebar-link>
-      </template>
+      <sidebar-link to="/admin/comments">
+        <i class="nc-icon nc-quote"></i>
+        <p>Comentários</p>
+      </sidebar-link>
     </side-bar>
+
+    <!-- main dashboard painel -->
     <div class="main-panel">
-      <top-navbar></top-navbar>
+      <top-navbar>
+      </top-navbar>
 
       <dashboard-content @click="toggleSidebar">
-
       </dashboard-content>
 
-      <content-footer></content-footer>
+      <content-footer>
+      </content-footer>
     </div>
   </div>
 </template>
-<style lang="scss">
-
-</style>
 <script>
-  import TopNavbar from './TopNavbar.vue'
-  import ContentFooter from './ContentFooter.vue'
-  import DashboardContent from './Content.vue'
-  import MobileMenu from './MobileMenu.vue'
-  export default {
-    components: {
-      TopNavbar,
-      ContentFooter,
-      DashboardContent,
-      MobileMenu
-    },
-    methods: {
-      toggleSidebar () {
-        if (this.$sidebar.showSidebar) {
-          this.$sidebar.displaySidebar(false)
-        }
+import TopNavbar from './TopNavbar.vue'
+import ContentFooter from './ContentFooter.vue'
+import DashboardContent from './Content.vue'
+import MobileMenu from './MobileMenu.vue'
+import SidebarLink from '../components/SidebarPlugin/SidebarLink.vue'
+
+export default {
+  components: {
+  TopNavbar,
+  ContentFooter,
+  DashboardContent,
+  MobileMenu,
+  SidebarLink
+},
+  methods: {
+    toggleSidebar () {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false)
       }
     }
   }
-
+}
 </script>
+
+<style lang="scss">
+</style>
