fcf1d1d38d1c0f811fffb40fdb63b4bc6c03d4a8
Paulo Sergio <pauloserg861@gmail.com>
2022-09-22 14:39:52 -0300

    criando rota de comentarios

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,7 +11,7 @@ import Typography from 'src/pages/Typography.vue'
 import Icons from 'src/pages/Icons.vue'
 import Maps from 'src/pages/Maps.vue'
 import Notifications from 'src/pages/Notifications.vue'
-import Upgrade from 'src/pages/Upgrade.vue'
+import CommentsView from 'src/pages/CommentsView.vue'
 
 const routes = [
   /*{
@@ -68,9 +68,9 @@ const routes = [
         component: Notifications
       },
       {
-        path: 'upgrade',
-        name: 'Upgrade to PRO',
-        component: Upgrade
+        path: 'comments',
+        name: 'Comments',
+        component: CommentsView
       }
     ]
   },
