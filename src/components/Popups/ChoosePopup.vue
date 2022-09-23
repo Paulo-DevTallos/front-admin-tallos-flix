9e535da9f94f7834d47ec2024016dd863a56d023
Paulo Sergio <pauloserg861@gmail.com>
2022-09-23 18:00:38 -0300

    criando popup de ações do usuario

---
+++ src/components/Popups/ChoosePopup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="box-modal">
+    <div>
+      Escolha sua ação
+    </div>
+    <div>
+      <button @click="$emit('delete_user')" class="btn btn-danger">Deletar usuario</button>
+      <button class="btn btn-warning">Atualizar usuario</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChoosePopup',
+  emits: ['delete_user']
+}  
+</script>
+
+<style>
+.box-modal {
+  position: absolute;
+  text-align: center;
+  top: 0;
+  background-color: #fff;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.264);
+  border-radius: 7px;
+  padding: 60px;
+}
+
+.box-modal button {
+  margin: 5px;
+}
+</style>
