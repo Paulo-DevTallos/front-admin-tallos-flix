bba156fd24eae93ae0655d26d1efc9b17f3c2e51
Paulo Sergio <pauloserg861@gmail.com>
2022-09-23 18:01:07 -0300

    criando renderização de usuario

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,37 +1,69 @@
 <template>
-  <table class="table">
-    <thead>
-      <slot name="columns">
-        <tr>
-          <th v-for="column in columns" :key="column">{{column}}</th>
-        </tr>
-      </slot>
-    </thead>
-    <tbody>
-    <tr v-for="(item, index) in data" :key="index">
-      <slot :row="item">
-        <td v-for="column in columns" :key="column" v-if="hasValue(item, column)">{{itemValue(item, column)}}</td>
-      </slot>
-    </tr>
-    </tbody>
-  </table>
+  <div>
+    <!--<div class="actions-btn">
+      <button type="button" class="btn btn-danger" @click="teste(item)">Deletar usuário</button>
+    </div>-->
+    <table class="table">
+      <thead>
+        <slot name="columns">
+          <tr>
+            <th v-for="column in columns" :key="column">{{column}}</th>
+          </tr>
+        </slot>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+        <slot :row="item">
+          <td v-for="column in columns" :key="column" v-if="hasValue(item, column)">{{itemValue(item, column)}}</td>
+          <td id="size-check">
+            <input type="checkbox" class="input-marker" @click="teste(item)" />
+          </td>
+        </slot>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
-  export default {
-    name: 'l-table',
-    props: {
-      columns: Array,
-      data: Array
-    },
-    methods: {
-      hasValue (item, column) {
-        return item[column.toLowerCase()] !== 'undefined'
-      },
-      itemValue (item, column) {
-        return item[column.toLowerCase()]
-      }
+export default {
+  name: 'l-table',
+  props: {
+    columns: Array,
+    data: Array,
+  },
+  data() {
+    return {
+      hidden_modal: false
     }
+  },
+  methods: {
+    hasValue (item, column) {
+      return item[column.toLowerCase()] !== 'undefined'
+    },
+    itemValue (item, column) {
+      return item[column.toLowerCase()]
+    },
+
+    teste(item) {
+      console.log(item._id)
+      this.hidden_modal = !this.hidden_modal
+    }
+  },
+  mounted() {
   }
+}
 </script>
 <style>
+#size-check {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+
+.input-marker {
+  height: 25px;
+  width: 15px;
+}
 </style>
--- src/pages/UsersReports.vue
+++ src/pages/UsersReports.vue
@@ -1,36 +1,66 @@
 <template>
-  <div>
-    Relatorio de usuarios da plataforma
-    <ul>
-      <li v-for="user in users" :key="user._id">{{ user.name }} - {{ user.email }}</li>
-    </ul>
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <card 
+            class="strpied-tabled-with-hover"
+            body-classes="table-full-width table-responsive"
+          >
+            <template slot="header">
+              <h4 class="card-title">Lista de usuários TallosFlix</h4>
+              <p class="card-category">Disponibilidade de usuários cadastrados na plataforma</p>
+            </template>
+            <l-table 
+              class="table-hover table-striped"
+              :columns="columns"
+              :data="users">
+            </l-table>
+            <choose-popup 
+              v-if="hidden_modal"
+            />
+          </card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
+import LTable from 'src/components/Table.vue'
+import Card from 'src/components/Cards/Card.vue'
 import Service from '../services/axios-requests'
+import ChoosePopup from '../components/Popups/ChoosePopup.vue'
+
+const tableColumns = ['Name', 'Email']
 
 export default {
-  name: 'UsersReports',
-  data() {
-    return {
-      users: [],
-    }
+  components: {
+    LTable,
+    Card,
+    ChoosePopup,
   },
-  methods: {
-    //list users
-    async listUser() {
-      await Service.listar().then(res => {
-        return JSON.stringify(this.users = res.data)
-      })
+  data () {
+    return {
+      columns: [...tableColumns],
+      users: [],
+      hidden_modal: false
     }
   },
 
+  methods: {
+    async listUsers() {
+      await Service.listar().then(res => {
+        return this.users = res.data
+      })
+    },
+  },
   mounted() {
-    this.listUser()
+    this.listUsers()
   }
 }
 </script>
-
 <style>
+.actions-btn button {
+  margin: 10px 15px;
+}
 </style>
