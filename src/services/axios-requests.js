ecb63abcb90031088025ceea7b2932143b8c5c48
Paulo Sergio <pauloserg861@gmail.com>
2022-09-26 18:05:05 -0300

    criando requisicoes

--- src/services/axios-requests.js
+++ src/services/axios-requests.js
@@ -5,6 +5,14 @@ export default {
     return http.get('/users')
   },
 
+  listarMovies: () => {
+    return http.get('/movies')
+  },
+
+  listarComments: () => {
+    return http.get('/comments')
+  },
+
   login: (data) => {
     return http.post('/login', data)
   },
