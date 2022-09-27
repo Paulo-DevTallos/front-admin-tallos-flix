0813befb9a861c79cde1ec23fead3730f8c8cb8b
Paulo Sergio <pauloserg861@gmail.com>
2022-09-27 14:19:07 -0300

    criando logs de usuarios

--- src/pages/LogsReports.vue
+++ src/pages/LogsReports.vue
@@ -1,14 +1,72 @@
 <template>
-  <div>
-      Relatorio de Logs
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <card
+            class="strpied-tabled-width-hover"
+            body-classes="table-full-width table-responsive"
+          >
+            <template slot="header">
+              <h4>Gerenciamento de Logs</h4>
+            </template>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th v-for="(column, index) in columns" :key="index">
+                    {{ column }}
+                  </th>
+                </tr>
+              </thead>
+              <tbody v-for="session in sessions" :key="session._id">
+                <tr>
+                  <td>{{ session.user_id }}</td>
+                  <td>
+                    <div id="actions-op">
+                      <i class="nc-icon nc-settings-gear-64"></i>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Service from '../services/axios-session.request'
+import Card from '../components/Cards/Card.vue'
+const tableColumns = ['E-mail', 'Acções da sessão']
 export default {
-  name: 'LogsReports',
+  name: "LogsReports",
+  components: { Card },
+  data() {
+    return {
+      columns: [...tableColumns],
+      sessions: []
+    };
+  },
+  methods: {
+    listAllSessions() {
+      Service.listSession().then(res => {
+        this.sessions = res.data;
+      });
+    }
+  },
+  mounted() {
+      this.listAllSessions();
+  },
 }  
 </script>
 
 <style>
+#actions-op {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
 </style>
