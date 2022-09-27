8431e41340abf1445e78f13408d4f0ac0cb51c95
Paulo Sergio <pauloserg861@gmail.com>
2022-09-27 16:43:35 -0300

    concluindo pagina de logs de usuarios

--- src/pages/LogsReports.vue
+++ src/pages/LogsReports.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="content">
-    <div class="container-fluid">
+  <div class="content page-height">
+    <div class="content container-fluid">
       <div class="row">
         <div class="col-12">
           <card
@@ -8,27 +8,18 @@
             body-classes="table-full-width table-responsive"
           >
             <template slot="header">
-              <h4>Gerenciamento de Logs</h4>
+              <h4 class="header-table">Gerenciamento de Logs</h4>
+              <p>Nesse ambiente é possível verificar os usuários que estão online na plataforma</p>
             </template>
-            <table class="table">
-              <thead>
-                <tr>
-                  <th class="header-table" v-for="(column, index) in columns" :key="index">
-                    {{ column }}
-                  </th>
-                </tr>
-              </thead>
-              <tbody v-for="session in sessions" :key="session._id">
-                <tr>
-                  <td>{{ session.user_id }}</td>
-                  <td>
-                    <div id="actions-op">
-                      <i class="nc-icon nc-settings-gear-64"></i>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          </card>
+          <card>
+            <ul class="list">
+              <li class="list-session" v-for="session in sessions" :key="session._id">
+                <div>
+                  {{ session.user_id }}
+                </div>
+              </li>
+            </ul>
           </card>
         </div>
       </div>
@@ -39,13 +30,12 @@
 <script>
 import Service from '../services/axios-session.request'
 import Card from '../components/Cards/Card.vue'
-const tableColumns = ['E-mail', 'Acções da sessão']
+
 export default {
   name: "LogsReports",
   components: { Card },
   data() {
     return {
-      columns: [...tableColumns],
       sessions: []
     };
   },
@@ -54,7 +44,7 @@ export default {
       Service.listSession().then(res => {
         this.sessions = res.data;
       });
-    }
+    },
   },
   mounted() {
       this.listAllSessions();
@@ -63,4 +53,30 @@ export default {
 </script>
 
 <style>
+.page-heigth {
+  height: 100vh;
+}
+
+.header-table {
+  margin: 0 0 15px;
+}
+
+.list {
+  padding: 0;
+}
+
+.list-session {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.139);
+  border-radius: 5px;
+  margin: 3px 0;
+  transition: .3s ease-in;
+}
+
+.list-session:hover {
+  background-color: #00000005;
+}
 </style>
