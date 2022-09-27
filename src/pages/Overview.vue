482ee0149b7f42a821184dd7ecf5b4042d53bf33
Paulo Sergio <pauloserg861@gmail.com>
2022-09-27 15:18:47 -0300

    consumindo dados do estado global

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -5,10 +5,10 @@
         <div class="col-xl-3 col-md-6">
           <stats-card>
             <div slot="header" class="icon-warning">
-              <i class="nc-icon nc-chart text-warning"></i>
+              <i class="nc-icon nc-single-02 text-warning"></i>
             </div>
             <div slot="content">
-              <p class="card-category">Capacityola</p>
+              <p class="card-category">Usuários na plataforma</p>
               <h4 class="card-title">{{ $store.state.users.length }}</h4>
             </div>
             <div slot="footer">
@@ -20,11 +20,11 @@
         <div class="col-xl-3 col-md-6">
           <stats-card>
             <div slot="header" class="icon-success">
-              <i class="nc-icon nc-light-3 text-success"></i>
+              <i class="nc-icon nc-tv-2 text-success"></i>
             </div>
             <div slot="content">
-              <p class="card-category">Revenue</p>
-              <h4 class="card-title">$1,345</h4>
+              <p class="card-category">Filmes disponíveis</p>
+              <h4 class="card-title">{{ $store.state.movies.length }}</h4>
             </div>
             <div slot="footer">
               <i class="fa fa-calendar-o"></i>Last day
@@ -35,10 +35,10 @@
         <div class="col-xl-3 col-md-6">
           <stats-card>
             <div slot="header" class="icon-danger">
-              <i class="nc-icon nc-vector text-danger"></i>
+              <i class="nc-icon nc-quote text-danger"></i>
             </div>
             <div slot="content">
-              <p class="card-category">Errors</p>
+              <p class="card-category">Comentários</p>
               <h4 class="card-title">23</h4>
             </div>
             <div slot="footer">
@@ -170,103 +170,119 @@
   </div>
 </template>
 <script>
-  import ChartCard from 'src/components/Cards/ChartCard.vue'
-  import StatsCard from 'src/components/Cards/StatsCard.vue'
-  import LTable from 'src/components/Table.vue'
+import ChartCard from 'src/components/Cards/ChartCard.vue'
+import StatsCard from 'src/components/Cards/StatsCard.vue'
+import LTable from 'src/components/Table.vue'
 
-  export default {
-    components: {
-      LTable,
-      ChartCard,
-      StatsCard
-    },
-    data () {
-      return {
-        editTooltip: 'Edit Task',
-        deleteTooltip: 'Remove',
-        pieChart: {
-          data: {
-            labels: ['40%', '20%', '40%'],
-            series: [40, 20, 40]
+export default {
+  components: {
+    LTable,
+    ChartCard,
+    StatsCard
+  },
+  data () {
+    return {
+      users: [],
+      movies: [],
+      editTooltip: 'Edit Task',
+      deleteTooltip: 'Remove',
+      pieChart: {
+        data: {
+          labels: ['40%', '20%', '40%'],
+          series: [40, 20, 40]
+        }
+      },
+      lineChart: {
+        data: {
+          labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
+          series: [
+            [287, 385, 490, 492, 554, 586, 698, 695],
+            [67, 152, 143, 240, 287, 335, 435, 437],
+            [23, 113, 67, 108, 190, 239, 307, 308]
+          ]
+        },
+        options: {
+          low: 0,
+          high: 800,
+          showArea: false,
+          height: '245px',
+          axisX: {
+            showGrid: false
+          },
+          lineSmooth: true,
+          showLine: true,
+          showPoint: true,
+          fullWidth: true,
+          chartPadding: {
+            right: 50
           }
         },
-        lineChart: {
-          data: {
-            labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-            series: [
-              [287, 385, 490, 492, 554, 586, 698, 695],
-              [67, 152, 143, 240, 287, 335, 435, 437],
-              [23, 113, 67, 108, 190, 239, 307, 308]
-            ]
-          },
-          options: {
-            low: 0,
-            high: 800,
-            showArea: false,
-            height: '245px',
+        responsiveOptions: [
+          ['screen and (max-width: 640px)', {
             axisX: {
-              showGrid: false
-            },
-            lineSmooth: true,
-            showLine: true,
-            showPoint: true,
-            fullWidth: true,
-            chartPadding: {
-              right: 50
+              labelInterpolationFnc (value) {
+                return value[0]
+              }
             }
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
-              }
-            }]
+          }]
+        ]
+      },
+      barChart: {
+        data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          series: [
+            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
+            [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
           ]
         },
-        barChart: {
-          data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            series: [
-              [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-              [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-            ]
+        options: {
+          seriesBarDistance: 10,
+          axisX: {
+            showGrid: false
           },
-          options: {
-            seriesBarDistance: 10,
+          height: '245px'
+        },
+        responsiveOptions: [
+          ['screen and (max-width: 640px)', {
+            seriesBarDistance: 5,
             axisX: {
-              showGrid: false
-            },
-            height: '245px'
-          },
-          responsiveOptions: [
-            ['screen and (max-width: 640px)', {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc (value) {
-                  return value[0]
-                }
+              labelInterpolationFnc (value) {
+                return value[0]
               }
-            }]
-          ]
-        },
-        tableData: {
-          data: [
-            {title: 'Sign contract for "What are conference organizers afraid of?"', checked: false},
-            {title: 'Lines From Great Russian Literature? Or E-mails From My Boss?', checked: true},
-            {
-              title: 'Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit',
-              checked: true
-            },
-            {title: 'Create 4 Invisible User Experiences you Never Knew About', checked: false},
-            {title: 'Read "Following makes Medium better"', checked: false},
-            {title: 'Unfollow 5 enemies from twitter', checked: false}
-          ]
-        }
+            }
+          }]
+        ]
+      },
+      tableData: {
+        data: [
+          {title: 'Sign contract for "What are conference organizers afraid of?"', checked: false},
+          {title: 'Lines From Great Russian Literature? Or E-mails From My Boss?', checked: true},
+          {
+            title: 'Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit',
+            checked: true
+          },
+          {title: 'Create 4 Invisible User Experiences you Never Knew About', checked: false},
+          {title: 'Read "Following makes Medium better"', checked: false},
+          {title: 'Unfollow 5 enemies from twitter', checked: false}
+        ]
       }
     }
+  },
+
+  methods: {
+    renderUsers() {
+      this.$store.dispatch('handleUsersRequest', this.users)
+    },
+    renderMovies() {
+      this.$store.dispatch('handleMoviesRequest', this.movies)
+    }
+  },
+
+  mounted() {
+    this.renderUsers()
+    this.renderMovies()
   }
+}
 </script>
 <style>
 
