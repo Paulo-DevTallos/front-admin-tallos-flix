22cb64262a9a835a2d6fe12f177174135abf9f9d
Paulo Sergio <pauloserg861@gmail.com>
2022-09-28 09:03:07 -0300

    tratando dos estilos da pagina de logs

--- src/pages/LogsReports.vue
+++ src/pages/LogsReports.vue
@@ -1,82 +1,52 @@
 <template>
-  <div class="content page-height">
-    <div class="content container-fluid">
-      <div class="row">
-        <div class="col-12">
-          <card
-            class="strpied-tabled-width-hover"
-            body-classes="table-full-width table-responsive"
-          >
-            <template slot="header">
-              <h4 class="header-table">Gerenciamento de Logs</h4>
-              <p>Nesse ambiente é possível verificar os usuários que estão online na plataforma</p>
-            </template>
-          </card>
-          <card>
-            <ul class="list">
-              <li class="list-session" v-for="session in sessions" :key="session._id">
-                <div>
-                  {{ session.user_id }}
-                </div>
-              </li>
-            </ul>
-          </card>
-        </div>
-      </div>
-    </div>
+  <div class="content">
+    <div class="container-fluid">
+      <card class="bg-color">
+        <template slot="header">
+          <h4 class="card-title">Gerenciamento de logs</h4>
+          <p class="card-category">Relatório de logs dos usuários logado na plataforma</p>
+        </template>
+        <ul class="report-content">
+          <li v-for="session in sessions" :key="session._id">{{ session.user_id }}</li>
+        </ul>
+      </card>
+    </div>      
   </div>
 </template>
 
 <script>
-import Service from '../services/axios-session.request'
 import Card from '../components/Cards/Card.vue'
-
+import Service from '../services/axios-session.request'
 export default {
-  name: "LogsReports",
   components: { Card },
+  name: 'LogsReports',
   data() {
     return {
-      sessions: []
-    };
+      sessions: [],
+    }
   },
   methods: {
-    listAllSessions() {
+    listAllUserSessions() {
       Service.listSession().then(res => {
-        this.sessions = res.data;
-      });
+        const parseSession = JSON.parse(JSON.stringify(res.data))
+        return this.sessions = parseSession
+      })
     },
   },
   mounted() {
-      this.listAllSessions();
-  },
-}  
+    this.listAllUserSessions()
+  }
+}
 </script>
 
-<style>
-.page-heigth {
-  height: 100vh;
-}
-
-.header-table {
-  margin: 0 0 15px;
-}
-
-.list {
-  padding: 0;
-}
-
-.list-session {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.139);
-  border-radius: 5px;
-  margin: 3px 0;
-  transition: .3s ease-in;
-}
-
-.list-session:hover {
-  background-color: #00000005;
-}
-</style>
+<style lang="scss" scoped>
+  .report-content {
+    padding: 10px 0;
+  }
+  .report-content li {
+    padding: 25px;
+    border-radius: 5px;
+    box-shadow: -2px 2px 5px rgba(0, 0, 0, 0.062);
+    margin: 4px;
+  }
+  </style>
