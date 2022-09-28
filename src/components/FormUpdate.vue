5ba2a9dbf944cf2d03e8863c5fb76bf348bf1e04
Paulo Sergio <pauloserg861@gmail.com>
2022-09-28 16:00:37 -0300

    implementando Animacao para form de usuario

--- src/components/FormUpdate.vue
+++ src/components/FormUpdate.vue
@@ -69,6 +69,23 @@ export default {
   transform: translate(-50%, -50%);
   box-shadow: -2px 2px 5px rgba(0, 0, 0, 0.342);
   padding: 30px;
+  animation: blow .5s ease-in-out;
+}
+
+@keyframes blow {
+  0% {
+    opacity: 0;
+    visibility: hidden;
+    transform: scale(0.0) translate(-50%, -50%);
+  }
+  70% {
+    opacity: 1;
+    visibility: visible;
+    transform: scale(1.2) translate(-50%, -50%);
+  }
+  100% {
+    transform: scale(1.1) translate(-50%, -50%);
+  }
 }
 
 .icon-update {
