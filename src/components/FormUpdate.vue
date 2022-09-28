cca894bc26a4519616cd30998fd37f217c84cf9e
Paulo Sergio <pauloserg861@gmail.com>
2022-09-28 10:49:03 -0300

    criando formulario de update

---
+++ src/components/FormUpdate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content card form-content">
+    <div>
+      <font-awesome-icon icon="fa-solid fa-file-pen" />
+    </div>
+    <form> 
+      <base-input />
+      <base-input />
+      <button>Alterar Dados</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import BaseInput from './Inputs/BaseInput.vue';
+export default {
+    name: "FormUpdate",
+    components: { BaseInput }
+}
+</script>
+
+<style>
+.form-content {
+  position: fixed;
+  top: 0;
+  left: 400px;
+}
+</style>
