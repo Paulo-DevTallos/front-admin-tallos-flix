5db13051ce2b6678794cfc2a6c9b90584c86f227
Paulo Sergio <pauloserg861@gmail.com>
2022-09-29 12:13:53 -0300

    criando modal de acoes dos filmes

---
+++ src/components/Popups/OptionPopup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content card option-content">
+    Escolha uma ação para realizar
+    <div>
+      <button>Atualizar</button>
+      <button>Excluir Filme</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OptionPopup',
+}
+</script>
+
+<style scoped>
+.option-content {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+}
+</style>
