1a2f73378c9aff95c889ce20eea8c6e38053403a
Paulo Sergio <pauloserg861@gmail.com>
2022-09-29 17:15:13 -0300

    parametrizando baseurl

--- src/services/http.js
+++ src/services/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: `http://localhost:3008`
+  baseURL: `http://localhost:3008`,
   //baseURL: `http://150.230.78.209:18000/app`
 })
