18e473c153ec3452400de95d34a5f686c03ac0fd
Paulo Sergio <pauloserg861@gmail.com>
2022-09-30 09:02:59 -0300

    estilizando actions bar

--- src/components/ActionsBar.vue
+++ src/components/ActionsBar.vue
@@ -2,10 +2,11 @@
   <div class="content">
     <base-input   
       v-model="name"
+      class="input-lg"
       placeholder="Encontre um usuário"
     />
-    <button>Pesquisa</button>
-    <button>
+    <button class="btn btn-style btn-primary">Pesquisa</button>
+    <button class="btn btn-style btn-primary">
       Adicionar novo usuário
       <font-awesome-icon icon="fa-solid fa-plus" />
     </button>
@@ -26,5 +27,30 @@ export default {
 </script>
 
 <style scoped>
+.content {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
 
+.input-lg {
+  width: 400px;
+}
+
+.btn-style {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 5px;
+  background-color: #0044aa;
+}
+
+.btn-style, .btn-style svg {
+  color: #fff;
+}
+
+.btn-style svg {
+  margin-left: 10px;
+  font-weight: bold;
+}
 </style>
