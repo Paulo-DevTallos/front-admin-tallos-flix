3a01cdd457d4b964b32c973f8f851e95e55933a2
Paulo Sergio <pauloserg861@gmail.com>
2022-09-30 08:55:57 -0300

    criando actions bar

---
+++ src/components/ActionsBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content">
+    <base-input   
+      v-model="name"
+      placeholder="Encontre um usuário"
+    />
+  </div>
+</template>
+
+<script>
+import BaseInput from './Inputs/BaseInput.vue'
+export default {
+  components: { BaseInput },
+  name: 'ActionsBar',
+  data() {
+    return {
+      name: ''
+    }
+  }
+}  
+</script>
+
+<style scoped>
+
+</style>
